169fa87246a7f2f3d1888efbb1cb1256bf05685d
chiller <egalaczi_business@yahoo.com>
2011-04-21 10:44:38 +0200

    Basic Client Activity that calls Service in other package

--- src/com/example/cryptoclient/SignerClient.java
+++ src/com/example/cryptoclient/SignerClient.java
@@ -1,7 +1,9 @@
 package com.example.cryptoclient;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SignerClient extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,13 @@ public class SignerClient extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        Intent intent = new Intent("org.axades.signer.START_CORE_SERVICE");
+        intent.putExtra("p12path", "kspath");
+        intent.putExtra("p12pass", "password");
+        intent.putExtra("signdata", "path");
+        startService(intent);
+        Log.v("test","Intent created");
     }
 }
