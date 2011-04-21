4e621ea999cb22bd79fafe3d1b79b8b469b1ebf8
chiller <egalaczi_business@yahoo.com>
2011-04-21 14:15:52 +0200

    SignerClient with resultreceiver, doesnt seem to work

--- src/com/example/cryptoclient/SignerClient.java
+++ src/com/example/cryptoclient/SignerClient.java
@@ -1,5 +1,7 @@
 package com.example.cryptoclient;
 
+import java.util.Currency;
+
 import com.example.filechooser.FileChooser;
 
 import android.app.Activity;
@@ -8,18 +10,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.ResultReceiver;
 import android.util.Log;
 import android.widget.Toast;
 
 public class SignerClient extends Activity {
 	
-	private BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // TODO Auto-generated method stub
-            Log.v("client", intent.getStringExtra("result"));
-        }
-    };
+	
+    
+
 	private int requestCode;
 	
 	
@@ -28,41 +29,44 @@ public class SignerClient extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
-    
-    @Override
-	protected void onResume() {
-	    super.onResume();
-	    
-	    Intent i = new Intent(this,FileChooser.class);      
-        i.putExtra("request","document");
-        startActivityForResult(i, requestCode);
         
+//        Intent i = new Intent(this,FileChooser.class);      
+//        i.putExtra("request","document");
+//        startActivityForResult(i, requestCode);
+//        
 	    
-	    IntentFilter filter = new IntentFilter();
-        String broadcastid = "servbc3";
-	    filter.addAction(broadcastid);
-        registerReceiver(receiver, filter);
+	    
 
         
         Intent intent = new Intent("org.axades.core.START_CORE_SERVICE");
         intent.putExtra("p12path", "kspath");
         intent.putExtra("p12pass", "password");
         intent.putExtra("signdata", "path");
-        intent.putExtra("broadcastid", broadcastid);
+        
+       
+        Handler h = Thread.currentThread().
+        intent.putExtra("resultreceiver", 
+        
+        		new ResultReceiver(null) {
+            @Override
+            protected void onReceiveResult(int resultCode, Bundle resultData) {
+                
+                    Log.v("client",resultData.getString("alma"));
+
+            }
+        }
+        
+        
+        );
         
         startService(intent);
-        
     }
     
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d("client","onActivityResult and resultCode = "+resultCode);
-        // TODO Auto-generated method stub
-        super.onActivityResult(requestCode, resultCode, data);
-            Toast.makeText(this, "OK", Toast.LENGTH_LONG).show();
-
+	protected void onResume() {
+	    super.onResume();
+        
     }
-    
+
     
 }
